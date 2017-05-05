bf2d65089a12c28ea247d37e957af93436704c3a
Miaolegemie <1942037006@qq.com>
2017-05-05 22:11:24 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 
-// import mock from './server/mock'
+import mock from './server/mock'
 
 // 开启debug模式
 Vue.config.debug = true
